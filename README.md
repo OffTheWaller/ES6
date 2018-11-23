9e8419e6f8841cee977b7ef9ec25d8ec78d1c38d
OffTheWaller <1031118178@qq.com>
2018-11-23 16:32:49 +0800

    es6

--- README.md
+++ README.md
@@ -223,3 +223,35 @@ let list = arr.entries();
 console.log(list)  //Array Iterator {}
 console.log(list.next().value)  //[0, "hello"]
 ```
+
+### 箭头函数
+```javascript
+es6中主动抛出错误,es6中并且可以抛出默认值
+function add (a, b=1) {
+    if (a == 0) {
+        throw new Error('a can not be 0')
+    } else {
+        return a + b
+    }
+}
+console.log(add(0))
+
+如果形参使用了默认值，再使用'use strict'，就会报错，必须取消使用默认值
+
+es6使用 函数名.length可以获取到必须传入的参数个数
+function add (a, b=1) {
+    if (a == 0) {
+        throw new Error('a can not be 0')
+    } else {
+        return a + b
+    }
+}
+console.log(add.length)  //这里为1因为使用了默认值
+
+箭头函数
+var add = (a,b) => {
+    console.log('两句话必须加{}')
+    return a + b;
+}
+console.log(add(1,2))
+```
--- dist/index.js
+++ dist/index.js
@@ -1,63 +1,30 @@
 'use strict';
 
-//json数组格式，利用Array.from()方法
-// let json = {
-//     "0": "name",
-//     "1": "age",
-//     "2": "sex",
-//     length: 3
+//es6中主动抛出错误,es6中并且可以抛出默认值
+// function add (a, b=1) {
+//     if (a == 0) {
+//         throw new Error('a can not be 0')
+//     } else {
+//         return a + b
+//     }
 // }
-// var arr = Array.from(json)
-// console.log(arr)
+// console.log(add(0))
 
-//这种形式的就不行，属性名必须是0,1,2这样的索引形式，并且必须有length属性
-// let json = {
-//     "name": "jack",
-//     "age": "18",
-//     "sex": "male",
-//     length: 3
+//如果形参使用了默认值，再使用'use strict'，就会报错，必须取消使用默认值
+
+//es6使用 函数名.length可以获取到必须传入的参数个数
+// function add (a, b=1) {
+//     if (a == 0) {
+//         throw new Error('a can not be 0')
+//     } else {
+//         return a + b
+//     }
 // }
-// var arr = Array.from(json)
-// console.log(arr)
+// console.log(add.length)  //这里为1因为使用了默认值
 
-//Array.of()方法，把传入的数字或者字符串转换成数组
-// var arr = Array.of(1,2,3,4,5)
-// console.log(arr)
-
-// var arrStr = Array.of('hello', 'es6', 'world')
-// console.log(arrStr)
-
-//find()实例方法，实例方法不是Array的方法，是一个真实数组的方法
-// let arr=[1,2,3,4,5,6,7,8,9];
-// console.log(arr.find(function(value,index,arr){
-//     return value > 5;
-// }))
-
-//fill()实例方法
-// let arr = [1,2,3,4,5,6];
-// arr.fill('hello',1,4)
-// console.log(arr)  //[1, "hello", "hello", "hello", 5, 6]
-
-//for of循环
-// let arr = ['hello','world','es6','vue'];
-// for (let item of arr) {
-//     console.log(item)
-// }
-
-//打出索引
-// let arr = ['hello','world','es6','vue'];
-// for (let index of arr.keys()) {
-//     console.log(index)
-// }
-
-//同时打出索引和值，用entries()方法
-// let arr = ['hello','world','es6','vue'];
-// for (let [index,item] of arr.entries()) {
-//     console.log(index+":"+item)
-// }
-
-//entries()返回一个iterator迭代器，可以手动使用next()
-var arr = ['hello', 'world', 'es6', 'vue'];
-var list = arr.entries();
-console.log(list); //Array Iterator {}
-console.log(list.next().value[1]); //[0, "hello"]
+//箭头函数
+var add = function add(a, b) {
+    console.log('两句话必须加{}');
+    return a + b;
+};
+console.log(add(1, 2));
