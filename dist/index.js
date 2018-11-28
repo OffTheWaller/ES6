efebd204a77de9726036b624cd914a09d6809b59
OffTheWaller <1031118178@qq.com>
2018-11-28 14:12:20 +0800

    es6

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,5 @@
 'use strict';
 
-//Symbol
-// let a = Symbol('hello')
-// console.log(a)  //红色
-// console.log(a.toString())  //黑色
-
-//Symbol当做对象的key
-// let key = Symbol();
-// var obj = {
-//     [key]: 'javascript'
-// }
-// console.log(obj[key])
-// obj[key] = 'html'
-// console.log(obj[key])
-
-//Symbol对对象属性的保护
-var obj = {
-    name: 'zhang'
-};
-var age = Symbol();
-obj[age] = 18;
-console.log(obj);
-console.log('----------------');
-for (var item in obj) {
-    console.log(item);
-}
+//set 和 map
+var set1 = new Set(['hello', 'world']);
+console.log(set1);
---
+++ src/index.js
@@ -0,0 +1,13 @@
+//set 和 map
+var set1 = new Set(['hello','world'])
+console.log(set1)
+
+/**
+ * set的方法有：add() delete() has() clear() for of和forEach循环
+ */
+
+ /**
+  * map的方法有 set()  get() has() delete() clear()
+  */
+
+//WeakSet只能传入对象
