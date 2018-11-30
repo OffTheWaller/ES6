eaa1ca1ae4710b5cff5eee46cd081d715613e0e3
OffTheWaller <1031118178@qq.com>
2018-11-30 11:20:47 +0800

    promise

--- README.md
+++ README.md
@@ -374,3 +374,43 @@ console.log(set1)
 
 //WeakSet只能传入对象
 ```
+
+### promise
+```javascript
+let state=1;
+function step1(resolve,reject){
+    console.log('1.开始-洗菜做饭');
+    if(state==1){
+        resolve('洗菜做饭--完成');
+    }else{
+        reject('洗菜做饭--出错');
+    }
+}
+function step2(resolve,reject){
+    console.log('2.开始-坐下来吃饭');
+    if(state==1){
+        resolve('坐下来吃饭--完成');
+    }else{
+        reject('坐下来吃饭--出错');
+    }
+}
+function step3(resolve,reject){
+    console.log('3.开始-收拾桌子洗完');
+     if(state==1){
+        resolve('收拾桌子洗完--完成');
+    }else{
+        reject('收拾桌子洗完--出错');
+    }
+}
+new Promise(step1).then(function(data){
+    console.log(data);
+    return new Promise(step2);
+}).then(function(data){
+     console.log(data);
+    return new Promise(step3);
+}).then(function(data){
+    console.log(data);
+    return data;
+});
+```
+- promise对象接受一个参数，这个参数是个处理函数，又有两个参数，一个是resolve函数，一个是reject函数，使用.then()里面接受两个参数，一个是resolve具体函数，一个是reject具体函数
--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
-//set 和 map
-var set1 = new Set(['hello','world'])
-console.log(set1)
-
-/**
- * set的方法有：add() delete() has() clear() for of和forEach循环
- */
-
- /**
-  * map的方法有 set()  get() has() delete() clear()
-  */
-
-//WeakSet只能传入对象
+let state=1;
+function step1(resolve,reject){
+    console.log('1.开始-洗菜做饭');
+    if(state==1){
+        resolve('洗菜做饭--完成');
+    }else{
+        reject('洗菜做饭--出错');
+    }
+}
+function step2(resolve,reject){
+    console.log('2.开始-坐下来吃饭');
+    if(state==1){
+        resolve('坐下来吃饭--完成');
+    }else{
+        reject('坐下来吃饭--出错');
+    }
+}
+function step3(resolve,reject){
+    console.log('3.开始-收拾桌子洗完');
+     if(state==1){
+        resolve('收拾桌子洗完--完成');
+    }else{
+        reject('收拾桌子洗完--出错');
+    }
+}
+new Promise(step1).then(function(data){
+    console.log(data);
+    return new Promise(step2);
+}).then(function(data){
+     console.log(data);
+    return new Promise(step3);
+}).then(function(data){
+    console.log(data);
+    return data;
+});
